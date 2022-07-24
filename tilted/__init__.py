be99bce875dd0f5a52efd1e5b9d3f4d0ecd0b588
Max Atkinson <maxatkinson@live.com>
2022-07-24 21:43:28 +0100

    Bump version, update readme

--- tilted/__init__.py
+++ tilted/__init__.py
@@ -1,5 +1,5 @@
 from tilted.enums import CardRank, CardSuit
 from tilted.models import Card, Deck, Game, Hand
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __all__ = ("Card", "Deck", "Game", "Hand", "CardRank", "CardSuit")
